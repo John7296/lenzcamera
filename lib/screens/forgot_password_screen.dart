0e38c2f8ae262aaba75190ef8077b0e41c15abfe
ReshmaSathyan <reshusathyan@gmail.com>
2022-12-06 18:56:52 +0530

    validation done

--- lib/screens/forgot_password_screen.dart
+++ lib/screens/forgot_password_screen.dart
@@ -20,6 +20,8 @@ class ForgotPasswordScreen extends StatefulWidget {
 
 class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
 
+    final GlobalKey<FormState> _form = GlobalKey<FormState>();
+
   final _emailController = TextEditingController();
 
    bool emailSent = false;
@@ -32,7 +34,9 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
 
    void forgotPasswordOTPSend() {
 
-    
+      if (!_form.currentState!.validate()) {
+      return;
+    }
     NetworkManager.shared.forgotPasswordOTPSend(_emailController.text, "").then((BaseResponse response) {
       print(_emailController.text);
       setState(() {
@@ -77,79 +81,84 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
         body: Padding(
           padding: const EdgeInsets.only(left: 24, right: 24),
           child: SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(height: 200),
-                Center(
-                  child: Container(
-                      width: 220,
-                      child: Image(image: AssetImage("assets/images/logo_lenzcamera.png"))),
-                ),
-                SizedBox(height: 50),
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    "Email Or Mobile Number",
-                    style: TextStyle(
-                        color: Color(0xff747474),
-                        fontWeight: FontWeight.w600,
-                        fontSize: 12),
+            child: Form(
+              key: _form,
+              child: Column(
+                children: [
+                  SizedBox(height: 200),
+                  Center(
+                    child: Container(
+                        width: 220,
+                        child: Image(image: AssetImage("assets/images/logo_lenzcamera.png"))),
                   ),
-                ),
-                SizedBox(height: 12),
-                Container(
-                  height: 40,
-                  child: TextFormField(
-                     enabled: !emailSent,
-                      decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xffb0b0b0)),
+                  SizedBox(height: 50),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Email Or Mobile Number",
+                      style: TextStyle(
+                          color: Color(0xff747474),
+                          fontWeight: FontWeight.w600,
+                          fontSize: 12),
                     ),
-                    labelText: " ",
                   ),
-                   controller: _emailController,
-                                    validator: (val) {
-                                      if (isMobileNumber) {
-                                        if (val!.isEmpty)
-                                          return "Enter Mobile/Email";
-                                      } else {
-                                        if (!Helper.validateEmail(val!)) {
-                                          return "Please enter a valid email";
+                  SizedBox(height: 12),
+                  Container(
+                  //  height: 40,
+                    child: TextFormField(
+                       enabled: !emailSent,
+                        decoration: InputDecoration(
+                           contentPadding:
+                       EdgeInsets.symmetric(vertical: 2.0, horizontal: 2.0),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xffb0b0b0)),
+                      ),
+                      labelText: " ",
+                    ),
+                     controller: _emailController,
+                                      validator: (val) {
+                                        if (isMobileNumber) {
+                                          if (val!.isEmpty)
+                                            return "Enter Mobile/Email";
+                                        } else {
+                                          if (!Helper.validateEmail(val!)) {
+                                            return "Please enter a valid email";
+                                          }
                                         }
-                                      }
-
-                                      return null;
-                                    }               
-                  ),
-                ),
-                SizedBox(height: 25),
-              
-                Container(
-                  height: 40,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xffec3436),
+            
+                                        return null;
+                                      }               
                     ),
-                    onPressed: () {
-
-                        
-                          forgotPasswordOTPSend();
-                        
-                      // Navigator.push(
-                      // context,
-                      // MaterialPageRoute(
-                      // builder: (context) =>
-                      // OtpScreen()));
-          
-                    },
-                    child: Center(
-                        child: Text(
-                          "Send OTP",
-                          style: TextStyle(fontSize: 20, color: Colors.white),
-                        )),
                   ),
-                ),
-              ],
+                  SizedBox(height: 25),
+                
+                  Container(
+                    height: 40,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xffec3436),
+                      ),
+                      onPressed: () {
+            
+                          
+                            forgotPasswordOTPSend();
+                          
+                        // Navigator.push(
+                        // context,
+                        // MaterialPageRoute(
+                        // builder: (context) =>
+                        // OtpScreen()));
+                      
+                      },
+                      child: Center(
+                          child: Text(
+                            "Send OTP",
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          )),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
