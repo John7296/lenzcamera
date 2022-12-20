fbf8795e0886d20465c355ec76bda9551d4fa636
john7296 <johnchacko22@gmail.com>
2022-12-20 10:36:41 +0530

    cat changes

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -124,11 +124,11 @@ class _CategoryScreenState extends BaseStatefulState<CategoryScreen> {
               children: [
                 IconButton(
                   onPressed: () {
-                    Future.delayed(Duration(milliseconds: 500), () {
-                      getCategories();
-                    });
-                    // Navigator.push(context,
-                    //     MaterialPageRoute(builder: (context) => CartScreen()));
+                    // Future.delayed(Duration(milliseconds: 500), () {
+                    //   getCategories();
+                    // });
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => CartScreen()));
                     // getBanners();
                   },
                   icon: Icon(Icons.shopping_cart),
